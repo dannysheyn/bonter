8b4f73a352f7c2108b3c5d7a543ac6a64c5d9993
dannysheyn <danielsha@mta.ac.il>
2021-02-03 12:12:35 +0200

    added ts

--- backend-server/botsite/site_server/urls.py
+++ backend-server/botsite/site_server/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='bot-home'),
     path('about/', views.about, name='bot-about'),
+    path('dashboard/', views.dashboard, name='bot-dashboard'),
 ]
--- front-end/bot-front/src/sidebar.tsx
+++ front-end/bot-front/src/sidebar.tsx
@@ -2,13 +2,13 @@ import logo from './bot_logo_crop.jpg';
 
 const Sidebar = () => {
     const avatarWidth = 80;
-    const avatarHight = 80;
+    const avatarHeight = 100;
     const alt = 'bot logo';
 
     return (
         <div>
             <nav className="sidebar">
-                <img src={logo} alt={alt} width={avatarWidth} hight={avatarHight}/>
+                <img src={logo} alt={alt} width={avatarWidth} height={avatarHeight}/>
                 <div className="links">
                     <a href="/">Home</a>
                     <a href="/">Build</a>
